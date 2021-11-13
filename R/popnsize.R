45d631e43e91f5a2a19fbdb3e8d41a590ba00553
krennpa <77174075+krennpa@users.noreply.github.com>
2021-11-13 16:58:31 +0100

    added examples

---
+++ R/popnsize.R
@@ -0,0 +1,23 @@
+#' Demographic population-size data
+#'
+#' The data set is synthetic EU-SILC data based on the data set
+#' \code{\link[simFrame]{eusilcP}} from package \pkg{simFrame}. The data set is
+#' reduced to 17 variables containing three regional variables for the states
+#' and districts.
+#'
+#' @format A data frame with 94 observations and 2 variables:
+#' \describe{
+#' \item{district}{character; districts (94 levels).}
+#' \item{N_i}{numeric; population of district in 2006.}
+#' }
+#' @docType data
+"popnsize"
+
+
+# Population sizes for corresponding year of 2006
+# https://www.statistik.at/web_de/statistiken/menschen_und_gesellschaft/bevoelkerung/
+# bevoelkerungsstand_und_veraenderung/bevoelkerung_zu_jahres-_quartalsanfang/index.html
+
+
+
+
