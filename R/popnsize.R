c2ba87b349c49dbb149c358c4507af9cad4ab80a
krennpa <77174075+krennpa@users.noreply.github.com>
2021-11-13 17:14:28 +0100

    .

--- R/popnsize.R
+++ R/popnsize.R
@@ -1,11 +1,9 @@
 #' Demographic population-size data
 #'
-#' The data set is synthetic EU-SILC data based on the data set
-#' \code{\link[simFrame]{eusilcP}} from package \pkg{simFrame}. The data set is
-#' reduced to 17 variables containing three regional variables for the states
-#' and districts.
+#' The data set is summarized population data for Austrian districts in the year 2006. The original data
+#' is retrieved from https://www.statistik.at/web_de/statistiken/menschen_und_gesellschaft/bevoelkerung/bevoelkerungsstand_und_veraenderung/bevoelkerung_zu_jahres-_quartalsanfang/index.html
 #'
-#' @format A data frame with 94 observations and 2 variables:
+#' @format A data frame with 94 Austrian districts and corresponding population:
 #' \describe{
 #' \item{district}{character; districts (94 levels).}
 #' \item{N_i}{numeric; population of district in 2006.}
@@ -13,10 +11,6 @@
 #' @docType data
 "popnsize"
 
-
-# Population sizes for corresponding year of 2006
-# https://www.statistik.at/web_de/statistiken/menschen_und_gesellschaft/bevoelkerung/
-# bevoelkerungsstand_und_veraenderung/bevoelkerung_zu_jahres-_quartalsanfang/index.html
 
 
 
