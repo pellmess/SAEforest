72d6f0a6f52af9986ef12042a39519bbcab90adc
krennpa <77174075+krennpa@users.noreply.github.com>
2022-02-04 22:04:26 +0100

    added nonPara error-generation

--- R/MSE_SAEforest_mean_analytical.R
+++ R/MSE_SAEforest_mean_analytical.R
@@ -1,7 +1,44 @@
 # ADD error generation such as in the other Bootstraps (!)
 
 
-MSE_MERFanalytical <- function(mod, smp_data, X, dName, err_sd, B=25,
+# Is also in support functions..but use for now, as long as analytical is not fully integrated
+# in the package.
+
+ran_comp <- function(mod, smp_data, Y, dName, ADJsd){
+
+  forest_res1 <- Y - predict(mod$Forest, smp_data)$predictions
+  smp_data$forest_res <- forest_res1
+
+  # Random Effects
+  formRF <- formula(paste("forest_res ~", paste0(dName)))
+  ran_effs1 <- aggregate(data=smp_data, formRF, FUN=mean)
+  colnames(ran_effs1) <- c(dName,"r_bar")
+
+  smp_data <- dplyr::left_join(smp_data,ran_effs1,by = dName)
+  smp_data$forest_eij <- smp_data$forest_res-smp_data$r_bar
+
+  # prepare for sampling
+  forest_res <- smp_data$forest_eij
+  forest_res<-(forest_res/sd(forest_res))*ADJsd
+
+  # CENTER
+  forest_res <- forest_res-mean(forest_res)
+
+  # prepare for sampling
+  ran_effs <- ran_effs1$r_bar
+  ran_effs <- (ran_effs/sd(ran_effs))*mod$RanEffSD
+
+  # CENTER
+  ran_effs <- ran_effs-mean(ran_effs)
+
+  return(list(forest_res = forest_res,
+              ran_effs = ran_effs,
+              smp_data = smp_data))
+}
+
+
+
+MSE_MERFanalytical <- function(Y, mod, smp_data, X, dName, err_sd, B=25,
                                initialRandomEffects = 0, ErrorTolerance = 0.0001,
                                MaxIterations = 25, ...){
 
@@ -30,8 +67,12 @@ MSE_MERFanalytical <- function(mod, smp_data, X, dName, err_sd, B=25,
   pred_val <- matrix(predict(mod$Forest, smp_data)$predictions, ncol = B,
                      nrow = length(mod$Forest$predictions), byrow = FALSE)
 
-  y_hat <- pred_val + rnorm(sd=err_sd, n = length(pred_val))
-  u_i <- apply(y_hat, 2, function(x){rep(rnorm(sd=ran_sd, n = m_all), n_i)})
+  ran_obj <- ran_comp(Y=Y, smp_data = smp_data, mod=mod, ADJsd = err_sd, dName = dName)
+  ran_effs <- ran_obj$ran_effs
+  forest_res <- ran_obj$forest_res
+
+  y_hat <- pred_val + sample(forest_res, size = length(pred_val), replace = TRUE)
+  u_i <- apply(y_hat, 2, function(x){rep(sample(ran_effs, size = m_all, replace = TRUE), n_i)})
   y_star <- y_hat + u_i
 
   my_estim_f <- function(x){MERFranger(Y=x, X = X, random = rand_struc,
@@ -65,12 +106,7 @@ MSE_MERFanalytical <- function(mod, smp_data, X, dName, err_sd, B=25,
 
 
 
-
-
-
-
-
-MSE_MERFanalytical_Ni <- function(mod, smp_data, cens_data, X, dName, err_sd, B=25,
+MSE_MERFanalytical_Ni <- function(Y, mod, smp_data, cens_data, X, dName, err_sd, B=25,
                                   initialRandomEffects = 0, ErrorTolerance = 0.0001,
                                   MaxIterations = 25, ...){
 
@@ -102,8 +138,12 @@ MSE_MERFanalytical_Ni <- function(mod, smp_data, cens_data, X, dName, err_sd, B=
   pred_val_NI <- matrix(predict(mod$Forest, cens_data)$predictions, ncol = B,
                      nrow = dim(cens_data)[1], byrow = FALSE)
 
-  y_hat <- pred_val + rnorm(sd=err_sd, n = length(pred_val))
-  u_i <- apply(y_hat, 2, function(x){rep(rnorm(sd=ran_sd, n = m_all), n_i)})
+  ran_obj <- ran_comp(Y=Y, smp_data = smp_data, mod=mod, ADJsd = err_sd, dName = dName)
+  ran_effs <- ran_obj$ran_effs
+  forest_res <- ran_obj$forest_res
+
+  y_hat <- pred_val + sample(forest_res, size = length(pred_val), replace = TRUE)
+  u_i <- apply(y_hat, 2, function(x){rep(sample(ran_effs, size = m_all, replace = TRUE), n_i)})
   y_star <- y_hat + u_i
 
   my_estim_f <- function(x){MERFranger(Y=x, X = X, random = rand_struc,
@@ -141,6 +181,7 @@ MSE_MERFanalytical_Ni <- function(mod, smp_data, cens_data, X, dName, err_sd, B=
 
 
 #load("C:/Users/pkrennmair/Documents/MERF_aggSim_2408/s1_normal_500_aggForest.RData")
+#load("C:/Users/Patrick/Documents/Studium/FU Berlin/Diss/Data/Simulation_Data_AGGMERFPaper/MBsimu1008/s1_normal_500_aggForest.RData")
 
 #pop_data <- Pop[[15]]
 #smp_data <- samp[[15]]
@@ -156,12 +197,10 @@ MSE_MERFanalytical_Ni <- function(mod, smp_data, cens_data, X, dName, err_sd, B=
 #MSE_ni <- MSE_MERFanalytical(mod = model1$MERFmodel, smp_data = smp_data, X = X_covar,
 #                             dName = "idD", err_sd = model1$AdjustedSD, B=100)
 
-#MSE_Ni <- MSE_MERFanalytical_Ni(mod = model1$MERFmodel, cens_data= pop_data, smp_data = smp_data, X = X_covar,
+#MSE_Ni <- MSE_MERFanalytical_Ni(Y=income, mod = model1$MERFmodel, cens_data= pop_data, smp_data = smp_data, X = X_covar,
 #                                dName = "idD", err_sd = model1$AdjustedSD, B=100)
 
 
-#MSE_comp <- cbind(model1$MSE_Estimates$Mean, MSE_ni$Mean, MSE_Ni$Mean)
+#MSE_comp <- cbind(model1$MSE_Estimates$Mean, MSE_Ni$Mean, MSE_ni$Mean)
 #boxplot(sqrt(MSE_comp), col= 2:4)
 #summary(sqrt(MSE_comp))
-
-
